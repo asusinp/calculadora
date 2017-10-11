e5c375e1fc8af41a5ecdcbe0ac23f47e8274b691
iaw21752927 <alexsusin1998@gmail.com>
2017-10-11 20:07:40 +0200

    aaaaa

--- src/calculadora/CalculadoraApp.java
+++ src/calculadora/CalculadoraApp.java
@@ -8,10 +8,13 @@ import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class CalculadoraApp {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -34,6 +37,7 @@ public class CalculadoraApp {
 	 */
 	public CalculadoraApp() {
 		initialize();
+		circle
 	}
 
 	/**
@@ -43,16 +47,95 @@ public class CalculadoraApp {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JButton button0 = new JButton("0");
-		button0.addActionListener(new ActionListener() {
+		JButton button = new JButton("7");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		frame.getContentPane().add(button0, BorderLayout.WEST);
+		button.setBounds(12, 71, 67, 25);
+		frame.getContentPane().add(button);
 		
-		JButton button1 = new JButton("1");
-		frame.getContentPane().add(button1, BorderLayout.CENTER);
+		JButton btnNewButton = new JButton("8");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(109, 71, 67, 25);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("9");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(207, 71, 67, 25);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("4");
+		btnNewButton_2.setBounds(12, 108, 67, 25);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("5");
+		btnNewButton_3.setBounds(109, 108, 67, 25);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("6");
+		btnNewButton_4.setBounds(207, 108, 67, 25);
+		frame.getContentPane().add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("1");
+		btnNewButton_5.setBounds(12, 145, 67, 25);
+		frame.getContentPane().add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("2");
+		btnNewButton_6.setBounds(109, 145, 67, 25);
+		frame.getContentPane().add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("3");
+		btnNewButton_7.setBounds(207, 145, 67, 25);
+		frame.getContentPane().add(btnNewButton_7);
+		
+		JButton button_1 = new JButton("0");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_1.setBounds(12, 182, 67, 25);
+		frame.getContentPane().add(button_1);
+		
+		JButton btnNewButton_8 = new JButton("=");
+		btnNewButton_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_8.setBounds(109, 182, 165, 25);
+		frame.getContentPane().add(btnNewButton_8);
+		
+		JButton btnNewButton_9 = new JButton("/");
+		btnNewButton_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_9.setBounds(305, 71, 117, 25);
+		frame.getContentPane().add(btnNewButton_9);
+		
+		JButton btnNewButton_10 = new JButton("*");
+		btnNewButton_10.setBounds(305, 108, 117, 25);
+		frame.getContentPane().add(btnNewButton_10);
+		
+		JButton btnNewButton_11 = new JButton("+");
+		btnNewButton_11.setBounds(305, 145, 117, 25);
+		frame.getContentPane().add(btnNewButton_11);
+		
+		JButton btnNewButton_12 = new JButton("-");
+		btnNewButton_12.setBounds(305, 182, 117, 25);
+		frame.getContentPane().add(btnNewButton_12);
+		
+		textField = new JTextField();
+		textField.setBounds(12, 12, 411, 45);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 	}
-
 }
